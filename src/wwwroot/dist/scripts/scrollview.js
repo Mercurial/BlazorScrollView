e527eddf9455b5aed291331078447fb9a1d51633
Clark Alesna <clark_alesna@hotmail.com>
2020-01-20 04:18:15 +0800

    First version of scroll view logic

--- src/wwwroot/dist/scripts/scrollview.js
+++ src/wwwroot/dist/scripts/scrollview.js
@@ -7,19 +7,79 @@ var BlazorScrollView;
         ScrollViewInterop.InitializeScrollView = function (scrollContainer) {
             var scrollHandleElement = document.createElement("div");
             scrollHandleElement.classList.add("handle");
-            scrollContainer.appendChild(scrollHandleElement);
+            var scrollHandleContainerElement = document.createElement("div");
+            scrollHandleContainerElement.classList.add("handle-container");
+            scrollHandleContainerElement.appendChild(scrollHandleElement);
+            scrollContainer.appendChild(scrollHandleContainerElement);
             ScrollViewInterop.SetScrollHandleHeight(scrollContainer);
+            ScrollViewInterop.InitializeGlobalHandlers();
+        };
+        ScrollViewInterop.InitializeGlobalHandlers = function () {
+            if (!ScrollViewInterop.IsGlobalHandlersInitialized) {
+                ScrollViewInterop.IsGlobalHandlersInitialized = true;
+                document.addEventListener("mousedown", ScrollViewInterop.HandleMouseDown);
+                document.addEventListener("mousemove", ScrollViewInterop.HandleMouseMove);
+                document.addEventListener("mouseup", ScrollViewInterop.HandleMouseUp);
+            }
+        };
+        ScrollViewInterop.HandleMouseDown = function (e) {
+            var target = e.target;
+            if (target.classList.contains("handle")) {
+                e.preventDefault();
+                e.stopImmediatePropagation();
+                e.stopPropagation();
+                ScrollViewInterop.CurrentHandleElement = target;
+                ScrollViewInterop.CurrentHandleY = e.clientY;
+            }
+        };
+        ScrollViewInterop.HandleMouseMove = function (e) {
+            var _a;
+            if (ScrollViewInterop.CurrentHandleElement) {
+                e.preventDefault();
+                e.stopImmediatePropagation();
+                e.stopPropagation();
+                var handle = ScrollViewInterop.CurrentHandleElement;
+                var startY = ScrollViewInterop.CurrentHandleY;
+                var displacement = e.clientY - startY;
+                var scrollContainer = (_a = handle.parentElement) === null || _a === void 0 ? void 0 : _a.parentElement;
+                var handleContainer = handle.parentElement;
+                var vars = ScrollViewInterop.ExtractVariables(scrollContainer);
+                scrollContainer.scrollTop += displacement * (vars[1] / vars[0]);
+                var handleY = parseFloat(window.getComputedStyle(handle, null).top);
+                var handleH = parseFloat(window.getComputedStyle(handle, null).height);
+                var newHandleY = handleY + displacement;
+                newHandleY = newHandleY <= 0 ? 0 : newHandleY;
+                newHandleY = newHandleY >= vars[0] - handleH ? vars[0] - handleH : newHandleY;
+                handleContainer.style.top = scrollContainer.scrollTop + "px";
+                handle.style.top = newHandleY + "px";
+                ScrollViewInterop.CurrentHandleY = e.clientY;
+            }
+        };
+        ScrollViewInterop.HandleMouseUp = function (e) {
+            if (ScrollViewInterop.CurrentHandleElement) {
+                e.preventDefault();
+                e.stopImmediatePropagation();
+                e.stopPropagation();
+                ScrollViewInterop.CurrentHandleElement = null;
+            }
         };
         ScrollViewInterop.SetScrollHandleHeight = function (scrollContainer) {
-            var hv = parseFloat(window.getComputedStyle(scrollContainer, null).height);
-            var ht = scrollContainer.scrollHeight;
-            var hh = ScrollViewInterop.GetScrolLHandleHeight(hv, hv, ht);
+            var vars = ScrollViewInterop.ExtractVariables(scrollContainer);
+            var hv = vars[0];
+            var ht = vars[1];
+            var hh = ScrollViewInterop.GetScrollHandleHeight(hv, hv, ht);
             var handle = scrollContainer.querySelector(".handle");
             handle.style.height = hh + "px";
         };
-        ScrollViewInterop.GetScrolLHandleHeight = function (hv, hs, ht) {
+        ScrollViewInterop.GetScrollHandleHeight = function (hv, hs, ht) {
             return hv * hs / ht;
         };
+        ScrollViewInterop.ExtractVariables = function (scrollContainer) {
+            var hv = parseFloat(window.getComputedStyle(scrollContainer, null).height);
+            var ht = scrollContainer.scrollHeight;
+            return [hv, ht];
+        };
+        ScrollViewInterop.IsGlobalHandlersInitialized = false;
         return ScrollViewInterop;
     }());
     BlazorScrollView.ScrollViewInterop = ScrollViewInterop;
--- src/wwwroot/scripts/scrollview.ts
+++ src/wwwroot/scripts/scrollview.ts
@@ -1,25 +1,98 @@
 namespace BlazorScrollView {
     export class ScrollViewInterop {
+        public static CurrentHandleElement: HTMLDivElement | null;
+        public static CurrentHandleY: number;
+        public static IsGlobalHandlersInitialized = false;
+
         public static InitializeScrollView(scrollContainer: HTMLDivElement): void {
             let scrollHandleElement = document.createElement("div");
             scrollHandleElement.classList.add("handle");
 
-            scrollContainer.appendChild(scrollHandleElement);
+            let scrollHandleContainerElement = document.createElement("div");
+            scrollHandleContainerElement.classList.add("handle-container");
+
+            scrollHandleContainerElement.appendChild(scrollHandleElement);
+            scrollContainer.appendChild(scrollHandleContainerElement);
 
             ScrollViewInterop.SetScrollHandleHeight(scrollContainer);
+            ScrollViewInterop.InitializeGlobalHandlers();
+        }
+
+        private static InitializeGlobalHandlers() {
+            if (!ScrollViewInterop.IsGlobalHandlersInitialized) {
+                ScrollViewInterop.IsGlobalHandlersInitialized = true;
+                document.addEventListener("mousedown", ScrollViewInterop.HandleMouseDown);
+                document.addEventListener("mousemove", ScrollViewInterop.HandleMouseMove);
+                document.addEventListener("mouseup", ScrollViewInterop.HandleMouseUp);
+            }
+        }
+
+        private static HandleMouseDown(e: MouseEvent) {
+            var target = e.target as HTMLDivElement;
+
+            if (target.classList.contains("handle")) {
+                e.preventDefault();
+                e.stopImmediatePropagation();
+                e.stopPropagation();
+
+                ScrollViewInterop.CurrentHandleElement = target;
+                ScrollViewInterop.CurrentHandleY = e.clientY;
+            }
+        }
+
+        private static HandleMouseMove(e: MouseEvent) {
+            if (ScrollViewInterop.CurrentHandleElement) {
+                e.preventDefault();
+                e.stopImmediatePropagation();
+                e.stopPropagation();
+                var handle = ScrollViewInterop.CurrentHandleElement;
+                var startY = ScrollViewInterop.CurrentHandleY;
+                var displacement = e.clientY - startY;
+                var scrollContainer = handle.parentElement?.parentElement as HTMLDivElement;
+                var handleContainer = handle.parentElement as HTMLDivElement;
+                let vars = ScrollViewInterop.ExtractVariables(scrollContainer);
+
+                scrollContainer.scrollTop += displacement * (vars[1] / vars[0]);
+
+                var handleY = parseFloat(window.getComputedStyle(handle, null).top);
+                var handleH = parseFloat(window.getComputedStyle(handle, null).height);
+                var newHandleY = handleY + displacement;
+
+                newHandleY = newHandleY <= 0 ? 0 : newHandleY;
+                newHandleY = newHandleY >= vars[0] - handleH ? vars[0] - handleH : newHandleY;
+
+                handleContainer.style.top = `${scrollContainer.scrollTop}px`;
+                handle.style.top = `${newHandleY}px`;
+                ScrollViewInterop.CurrentHandleY = e.clientY;
+            }
+        }
+
+        private static HandleMouseUp(e: MouseEvent) {
+            if (ScrollViewInterop.CurrentHandleElement) {
+                e.preventDefault();
+                e.stopImmediatePropagation();
+                e.stopPropagation();
+                ScrollViewInterop.CurrentHandleElement = null;
+            }
         }
 
         private static SetScrollHandleHeight(scrollContainer: HTMLDivElement) {
-            let hv = parseFloat(window.getComputedStyle(scrollContainer, null).height);
-            let ht = scrollContainer.scrollHeight;
-            let hh = ScrollViewInterop.GetScrolLHandleHeight(hv, hv, ht);
+            let vars = ScrollViewInterop.ExtractVariables(scrollContainer);
+            let hv = vars[0];
+            let ht = vars[1];
+            let hh = ScrollViewInterop.GetScrollHandleHeight(hv, hv, ht);
             var handle = scrollContainer.querySelector(".handle") as HTMLDivElement;
             handle.style.height = `${hh}px`;
         }
 
-        private static GetScrolLHandleHeight(hv: number, hs: number, ht: number)
-        {
+        private static GetScrollHandleHeight(hv: number, hs: number, ht: number) {
             return hv * hs / ht;
+        }
+
+        private static ExtractVariables(scrollContainer: HTMLDivElement) {
+            let hv = parseFloat(window.getComputedStyle(scrollContainer, null).height);
+            let ht = scrollContainer.scrollHeight;
+            return [hv, ht];
         }
     }
 }
